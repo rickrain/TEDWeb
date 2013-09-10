028117f4f2b65bff5af7b770cfd50e56c7c1e7c3
rickrain <rickrain@hotmail.com>
2013-09-10 11:24:17 -0700

    Created customer model.

---
+++ TEDDemo.Web.Solution/TEDDemo.Web/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TEDDemo.Web.Models
+{
+    public class Customer
+    {
+        public string Id { get; set; }
+        public string CustomerName { get; set; }
+    }
+}
